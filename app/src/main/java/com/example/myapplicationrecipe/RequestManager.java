a27ce2e9d4091cc13843ccb7748f96f163bacc29
= <wafa.benyahia@esprit.tn>
2022-11-16 22:54:16 +0100

    commit add sqlite

--- app/src/main/java/com/example/myapplicationrecipe/RequestManager.java
+++ app/src/main/java/com/example/myapplicationrecipe/RequestManager.java
@@ -3,6 +3,8 @@ package com.example.myapplicationrecipe;
 import android.content.Context;
 
 import com.example.myapplicationrecipe.Listener.RandomRecipeResponseListener;
+import com.example.myapplicationrecipe.Listener.RecipeDetailsListener;
+import com.example.myapplicationrecipe.Models.DetailsRecipe;
 import com.example.myapplicationrecipe.Models.Root;
 
 import java.util.List;
@@ -13,6 +15,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public class RequestManager {
@@ -41,8 +44,35 @@ public class RequestManager {
             }
         });
    }
+   public void getDetailsRecipe(RecipeDetailsListener recipeDetailsListener,int id ){
+        CallRecipeDetails callRecipeDetails = retrofit.create(CallRecipeDetails.class);
+        Call<DetailsRecipe> call= callRecipeDetails.callRandomRecipe(id,context.getString(R.string.api_key));
+        call.enqueue(new Callback<DetailsRecipe>() {
+            @Override
+            public void onResponse(Call<DetailsRecipe> call, Response<DetailsRecipe> response) {
+                if(!response.isSuccessful()){
+
+                    recipeDetailsListener.didError(response.message());
+
+                }
+                recipeDetailsListener.didFetch(response.body(),response.message());
+            }
+
+            @Override
+            public void onFailure(Call<DetailsRecipe> call, Throwable t) {
+                recipeDetailsListener.didError(t.getMessage());
+
+            }
+        });
+   }
+
     private interface  CallRandomRecipes{
         @GET("recipes/random")
         Call<Root> callRandomRecipe (@Query("apiKey") String apiKey, @Query("number") String number , @Query("tags")List<String> tags);
+    }
+    private interface CallRecipeDetails{
+        @GET("recipes/{id}/information")
+        Call<DetailsRecipe> callRandomRecipe ( @Path("id") int id,@Query("apiKey") String apiKey );
+
     }
 }
